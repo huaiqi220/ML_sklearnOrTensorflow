41ef90502c0035325b65f8ceb619761ba620485a
zhuziyang <zhuziyang123456@gmail.com>
2021-02-04 22:44:31 +0800

    决策树

---
+++ fesf.py
@@ -0,0 +1,7 @@
+
+
+a = input("你的分数?")
+print("你是一般大学") if int(a) >= 600 else print("大学还可以")
+
+
+
--- logisti_flower.py
+++ logisti_flower.py
@@ -14,7 +14,7 @@ log_reg.fit(x,y)
 x_new = np.linspace(0,3,1000).reshape(-1,1)
 y_proba = log_reg.predict_proba(x_new)
 
-plt.plot(x_new,y_proba[:,1],"g--",label="Iris-Virginica")
+plt.plot(x_new,y_proba[:,1],"g-",label="Iris-Virginica")
 plt.plot(x_new,y_proba[:,0],"b--",label="Not Iris")
 
 plt.show()
